a79f02a59e09c79a2acf52c7c4c3568a1915ce1b
DimaKirdanov <108092906+DimaKirdanov@users.noreply.github.com>
2023-01-16 21:18:38 +0200

    1-st hw task

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,51 @@
+
+import PropTypes from 'prop-types';
+import styles from "./profile.module.css";
+
+export default function Profile ({ username, tag, location, avatar, stats })  { 
+ 
+    return (
+        <>
+            <div className={styles.profile}>
+  <div className={styles.description}>
+    <img
+      src={avatar}
+      alt="User avatar"
+      className={styles.avatar}
+    />
+            <p className={styles.username}>{username}</p>
+    <p className={styles.tag}>{tag}</p>
+    <p className={styles.location}>{location}</p>
+  </div>
+
+  <ul className={styles.stats}>
+    <li className={styles.statsItem}>
+      <span className={styles.label}>Followers</span>
+      <span className={styles.quantity}>{stats.followers}</span>
+    </li>
+    <li className={styles.statsItem}>
+      <span className={styles.label}>Views</span>
+      <span className={styles.quantity}>{stats.views}</span>
+    </li>
+    <li className={styles.statsItem}>
+      <span className={styles.label}>Likes</span>
+      <span className={styles.quantity}>{stats.likes}</span>
+    </li>
+  </ul>
+</div>
+        </>
+    )
+}
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }).isRequired,
+};
+
+// export default Profile;
